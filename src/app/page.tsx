6fa6a571df2fb91defb2fb5fead261eb8f9721ca
Paweł Nąckiewicz <it.pnackiewicz@gmail.com>
2024-07-15 21:47:26 +0200

    add dev label

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <div className="h-full w-full py-4">
       <h1 className="text-6xl font-bold mt-10">Welcome to Next.js!</h1>
+      <p>This is dev branch</p>
     </div>
   )
 }
