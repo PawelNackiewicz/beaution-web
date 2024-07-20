b0d30649f4707d2feada14674183720b8cdaac1f
Paweł Nąckiewicz <it.pnackiewicz@gmail.com>
2024-07-20 13:23:03 +0200

    cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ export default function Home() {
 	return (
 		<div className="h-full w-full py-4">
 			<h1 className="mt-10 text-6xl font-bold">Welcome to Next.js!</h1>
-			<p>Prod branch</p>
 		</div>
 	);
 }
